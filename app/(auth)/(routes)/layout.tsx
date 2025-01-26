b3419eefde1183463679708bb7e79c75db5908d2
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-26 01:51:04 -0600

    Adding design components for authentication with login and registration pages with clerk

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,15 @@
+import Logo from "@/components/Logo/Logo";
+import React from "react";
+
+export default function LayoutAuth({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col justify-center h-full items-center">
+      <Logo />
+      {children}
+    </div>
+  );
+}
