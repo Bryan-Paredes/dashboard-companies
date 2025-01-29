8010675be4631240f2ff2b43d74263c07e162c77
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-29 16:45:39 -0600

    Modify the DELETE handler in the event route to use a promise instead of a direct object for eventId extraction.

--- app/api/event/[eventId]/route.ts
+++ app/api/event/[eventId]/route.ts
@@ -2,11 +2,11 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req: Request, { params }: { params: { eventId: string } }) {
+export async function DELETE(req: Request, { params }: { params: Promise<{ eventId: string }> }) {
     try {
 
         const userId = (await auth()).userId;
-        const { eventId } = params;
+        const eventId = (await params).eventId
 
 
         if (!userId) new NextResponse('Unauthorized', { status: 401 });
