d2410a886513c9580f5088eb7561bcbc91c3fc8d
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-29 16:43:16 -0600

    Modify the PATCH and DELETE handlers in the company route to use a promise instead of a direct object for extracting the companyId.

--- app/api/company/[companyId]/route.ts
+++ app/api/company/[companyId]/route.ts
@@ -2,10 +2,10 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req: Request, { params }: { params: { companyId: string } }) {
+export async function PATCH(req: Request, { params }: { params: Promise<{ companyId: string }> }) {
     try {
         const userId = (await auth()).userId;
-        const { companyId } = await params;
+        const companyId = (await params).companyId
         const values = await req.json();
 
         if (!userId) return new NextResponse('Unauthorized', { status: 401 })
@@ -31,10 +31,10 @@ export async function PATCH(req: Request, { params }: { params: { companyId: str
 }
 
 
-export async function DELETE(req: Request, { params }: { params: { companyId: string } }) {
+export async function DELETE(req: Request, { params }: { params: Promise<{ companyId: string }> }) {
     try {
         const userId = (await auth()).userId;
-        const { companyId } = await params;
+        const companyId = (await params).companyId
 
         if (!userId) return new NextResponse('Unauthorized', { status: 401 })
 
