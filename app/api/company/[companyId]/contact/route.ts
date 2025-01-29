4af80ee94ff0282b4fb24e12fd7e6af96075f1a6
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-29 16:32:53 -0600

    Refactor POST handler in contact route to simplify parameter extraction

--- app/api/company/[companyId]/contact/route.ts
+++ app/api/company/[companyId]/contact/route.ts
@@ -2,10 +2,10 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request, { params }: { params: { companyId: string } }) {
+export async function POST(req: Request, params: { params: { companyId: string } }) {
     try {
         const userId = (await auth()).userId;
-        const { companyId } = await params;
+        const { companyId } = params.params;
         const data = await req.json();
 
         if (!userId) return new NextResponse('Unauthorized', { status: 401 })
