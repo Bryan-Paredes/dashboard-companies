4f73bb8329a348b66803194ff580b9d0cf76fedf
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-29 18:56:44 -0600

    Actualizar la configuración de Prisma para usar el adaptador de Neon y agregar claves foráneas en la base de datos

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,28 @@
-import { PrismaClient } from '@prisma/client'
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool, neonConfig } from '@neondatabase/serverless';
+
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
+
+// Type definitions
+// declare global {
+//   var prisma: PrismaClient | undefined
+// }
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+export const db = globalThis.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV === 'development') globalThis.prisma = db;
+
+
 
 // declare global {
 //     // eslint-disable-next-line no-var
@@ -17,19 +41,19 @@ import { PrismaClient } from '@prisma/client'
 // export const db = global.prisma || new PrismaClient({ adapter });
 // if (process.env.NODE_ENV === 'development') global.prisma = db;
 
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
+// import { Pool, neonConfig } from '@neondatabase/serverless'
+// import { PrismaNeon } from '@prisma/adapter-neon'
 // import { PrismaClient } from '@prisma/client'
 // import dotenv from 'dotenv'
-import ws from 'ws'
+// import ws from 'ws'
 
-// dotenv.config()
-neonConfig.webSocketConstructor = ws
-const connectionString = `${process.env.DATABASE_URL}`
+// // dotenv.config()
+// neonConfig.webSocketConstructor = ws
+// const connectionString = `${process.env.DATABASE_URL}`
 
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
-export const db = new PrismaClient({ adapter })
+// const pool = new Pool({ connectionString })
+// const adapter = new PrismaNeon(pool)
+// export const db = new PrismaClient({ adapter })
 
 // export const db = globalThis.prisma || new PrismaClient();
 // if (process.env.NODE_ENV !== 'development') globalThis.prisma = db;
---
+++ prisma/migrations/20250130004727_update/migration.sql
@@ -0,0 +1,5 @@
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
